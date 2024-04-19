46550a5f78e5cb121d4635efde20084ffefdff92
Aiperi Ormonbekova <aiperi.ormonbekovna@gmail.com>
2024-04-19 14:42:03 +0600

    done

--- food_app/lib/view/second_view.dart
+++ food_app/lib/view/second_view.dart
@@ -13,25 +13,32 @@ class SecondView extends StatelessWidget {
     color: Colors.green,
     ),
     ),
-       // drawer: Drawer(
-      //   backgroundColor: Colors.blue,
-      //     child:ListView(
-      //       children:  [
-      //         const DrawerHeader(
-      //         decoration: BoxDecoration(
-      //         color: Colors.blue,
-      //         ),
-      //         child:Text("Flutter"), 
-      //          ), 
-      //          ListTile(title: const Text("first"),onTap: () {}, 
-      // ), 
-      //          ListTile(title:const  Text("first"),onTap: () {}, 
-      // ), 
-      //          ListTile(title:const  Text("first"),onTap: () {}, 
-      // ), 
-      //        ],
-      //       ),
-      //      ),
+       drawer: Drawer( 
+     surfaceTintColor: Colors.brown,
+          backgroundColor: Colors.red,
+          shape:const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(20.0),
+              bottomRight: Radius.circular(20.0),
+            ),
+          ),
+          child:ListView(
+            children:  [
+              const DrawerHeader(
+              decoration: BoxDecoration(
+              color: Colors.blue,
+              ),
+              child:Text("Flutter"), 
+               ), 
+               ListTile(title: const Text("first"),onTap: () {}, 
+      ), 
+               ListTile(title:const  Text("first"),onTap: () {}, 
+      ), 
+               ListTile(title:const  Text("first"),onTap: () {}, 
+      ), 
+             ],
+            ),
+           ),
     );
   }
 }
